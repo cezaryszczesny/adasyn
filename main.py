8fb85cf14fcba64d18ef92de2d031ea87e0d50a8
BooleanEnjoyer <259745@student.pwr.edu.pl>
2023-05-14 22:54:19 +0200

    G_mean, MCC

--- main.py
+++ main.py
@@ -7,61 +7,65 @@ from adasyn import Adasyn
 from testMethods import test_oversampling_method, test_implemented_adasyn, test_before_oversampling
 
 
-# Authors: Cezary Szczesny
+# Authors: Cezary Szczesny, Dominik Badora
 
-def print_results(method_name, accuracy_score, precision_score, f1_score, recall_score):
+def print_results(method_name, accuracy_score, precision_score, f1_score, recall_score, g_mean, mcc):
     print("----------------------")
     print("Test method: " + method_name)
     print("Accuracy: " + "{:.3f}".format(accuracy_score))
     print("Precision:" + "{:.3f}".format(precision_score))
     print("F1: " + "{:.3f}".format(f1_score))
     print("Recall: " + "{:.3f}".format(recall_score))
+    print("G_mean: " + "{:.3f}".format(g_mean))
+    print("MCC: " + "{:.3f}".format(mcc))
 
 
-def save_results(method_name, accuracy_score, precision_score, f1_score, recall_score):
+def save_results(method_name, accuracy_score, precision_score, f1_score, recall_score, g_mean, mcc):
     np.save('accuracy_' + method_name + '.npy', accuracy_score)
     np.save('precision_' + method_name + '.npy', precision_score)
     np.save('f1_' + method_name + 'npy', f1_score)
     np.save('recall_' + method_name + '.npy', recall_score)
+    np.save('g_mean_' + method_name + '.npy', g_mean)
+    np.save('mcc_' + method_name + '.npy', mcc)
 
 
 # Generate samples
 X, y = make_classification(n_samples=500, weights=[0.9])
 
 # Before oversampling
-accuracy_score, precision_score, f1_score, recall_score = test_before_oversampling(X, y)
+accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score = test_before_oversampling(X, y)
 # save_results("Before oversampling", accuracy_score, precision_score, f1_score, recall_score)
-print_results("Before oversampling", accuracy_score, precision_score, f1_score, recall_score)
+print_results("Before oversampling", accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score)
 
 # Oversampling with implemented Adasyn
 adasyn = Adasyn()
-accuracy_score, precision_score, f1_score, recall_score = test_implemented_adasyn(X, y, adasyn)
+accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score = test_implemented_adasyn(X, y, adasyn)
 # save_results("Implemented Adasyn", accuracy_score, precision_score, f1_score, recall_score)
-print_results("Implemented Adasyn", accuracy_score, precision_score, f1_score, recall_score)
+print_results("Implemented Adasyn", accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score)
 
 # Oversampling with imported Adasyn
 ADASYN = ADASYN()
-accuracy_score, precision_score, f1_score, recall_score = test_oversampling_method(X, y, ADASYN)
+accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score = test_oversampling_method(X, y, ADASYN)
 # save_results("Imported Adasyn", accuracy_score, precision_score, f1_score, recall_score)
-print_results("Imported Adasyn", accuracy_score, precision_score, f1_score, recall_score)
+print_results("Imported Adasyn", accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score)
 
 # Oversampling with SMOTE
 SMOTE = SMOTE()
-accuracy_score, precision_score, f1_score, recall_score = test_oversampling_method(X, y, SMOTE)
+accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score = test_oversampling_method(X, y, SMOTE)
 # ("SMOTE", accuracy_score, precision_score, f1_score, recall_score)
-print_results("SMOTE", accuracy_score, precision_score, f1_score, recall_score)
+print_results("SMOTE", accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score)
 
 # Oversampling with SMOTE-ENN
 SMOTE_ENN = SMOTEENN()
-accuracy_score, precision_score, f1_score, recall_score = test_oversampling_method(X, y, SMOTE_ENN)
+accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score = test_oversampling_method(X, y, SMOTE_ENN)
 # save_results("SMOTE-ENN", accuracy_score, precision_score, f1_score, recall_score)
-print_results("SMOTE-ENN", accuracy_score, precision_score, f1_score, recall_score)
+print_results("SMOTE-ENN", accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score)
 
 # Oversampling with SMOTE-BORDERLINE
 BORDERLINE_SMOTE = BorderlineSMOTE()
-accuracy_score, precision_score, f1_score, recall_score = test_oversampling_method(X, y, BORDERLINE_SMOTE)
+accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score = test_oversampling_method(X, y, BORDERLINE_SMOTE)
 # save_results("SMOTE-BORDERLINE", accuracy_score, precision_score, f1_score, recall_score)
-print_results("SMOTE-BORDERLINE", accuracy_score, precision_score, f1_score, recall_score)
+print_results("SMOTE-BORDERLINE", accuracy_score, precision_score, f1_score, recall_score, g_mean_score, mcc_score)
 
 print("----------------------")
 print("THE END")
--- testMethods.py
+++ testMethods.py
@@ -1,15 +1,18 @@
 import numpy
-from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score, matthews_corrcoef
+from imblearn.metrics import geometric_mean_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
 
 
-# Authors: Cezary Szczesny
+# Authors: Cezary Szczesny, Dominik Badora
 def test_oversampling_method(X, y, oversampling_method_instance):
     accuracies = []
     precisions = []
     f1_scores = []
     recall_scores = []
+    g_means_table = []
+    mcc_table = []
     knc = KNeighborsClassifier(n_neighbors=5)
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=56)
     for idx, (train_index, test_index) in enumerate(skf.split(X, y)):
@@ -24,21 +27,28 @@ def test_oversampling_method(X, y, oversampling_method_instance):
         precision = precision_score(y_test, y_predict)
         f1 = f1_score(y_test, y_predict)
         rec_score = recall_score(y_test, y_predict)
+        g_mean = geometric_mean_score(y_test, y_predict)
+        mcc = matthews_corrcoef(y_test, y_predict)
 
         accuracies.append(accuracy)
         precisions.append(precision)
         f1_scores.append(f1)
         recall_scores.append(rec_score)
+        g_means_table.append(g_mean)
+        mcc_table.append(mcc)
+
         return numpy.average(accuracies), numpy.average(precisions), numpy.average(f1_scores), numpy.average(
-            recall_scores)
+            recall_scores), numpy.average(g_means_table), numpy.average(mcc_table)
 
 
-# Authors: Cezary Szczesny
+# Authors: Cezary Szczesny, Dominik Badora
 def test_implemented_adasyn(X, y, oversampling_method_instance):
     accuracies = []
     precisions = []
     f1_scores = []
     recall_scores = []
+    g_means_table = []
+    mcc_table = []
     knc = KNeighborsClassifier(n_neighbors=5)
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=56)
     for idx, (train_index, test_index) in enumerate(skf.split(X, y)):
@@ -53,27 +63,33 @@ def test_implemented_adasyn(X, y, oversampling_method_instance):
         precision = precision_score(y_test, y_predict)
         f1 = f1_score(y_test, y_predict)
         rec_score = recall_score(y_test, y_predict)
+        g_mean = geometric_mean_score(y_test, y_predict)
+        mcc = matthews_corrcoef(y_test, y_predict)
 
         accuracies.append(accuracy)
         precisions.append(precision)
         f1_scores.append(f1)
         recall_scores.append(rec_score)
+        g_means_table.append(g_mean)
+        mcc_table.append(mcc)
+
         return numpy.average(accuracies), numpy.average(precisions), numpy.average(f1_scores), numpy.average(
-            recall_scores)
+            recall_scores), numpy.average(g_means_table), numpy.average(mcc_table)
 
 
-# Authors: Cezary Szczesny
+# Authors: Cezary Szczesny, Dominik Badora
 def test_before_oversampling(X, y):
     accuracies = []
     precisions = []
     f1_scores = []
     recall_scores = []
+    g_means_table = []
+    mcc_table = []
     knc = KNeighborsClassifier(n_neighbors=5)
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=56)
     for idx, (train_index, test_index) in enumerate(skf.split(X, y)):
         X_train, y_train = X[train_index], y[train_index]
         X_test, y_test = X[test_index], y[test_index]
-
         knc.fit(X_train, y_train)
         y_predict = knc.predict(X_test)
 
@@ -81,10 +97,15 @@ def test_before_oversampling(X, y):
         precision = precision_score(y_test, y_predict)
         f1 = f1_score(y_test, y_predict)
         rec_score = recall_score(y_test, y_predict)
+        g_mean = geometric_mean_score(y_test, y_predict)
+        mcc = matthews_corrcoef(y_test, y_predict)
 
         accuracies.append(accuracy)
         precisions.append(precision)
         f1_scores.append(f1)
         recall_scores.append(rec_score)
+        g_means_table.append(g_mean)
+        mcc_table.append(mcc)
+
         return numpy.average(accuracies), numpy.average(precisions), numpy.average(f1_scores), numpy.average(
-            recall_scores)
+            recall_scores), numpy.average(g_means_table), numpy.average(mcc_table)
