efa6e0dad630213c1da0b762f14e2b5c58b2f579
Cezary Szczesny <c.szczesny@streamsoft.pl>
2023-05-14 20:49:38 +0200

    dodanie zapisu wyników do plików

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from imblearn.combine import SMOTEENN
 from imblearn.over_sampling import ADASYN, SMOTE, BorderlineSMOTE
 from sklearn.datasets import make_classification
@@ -17,36 +18,49 @@ def print_results(method_name, accuracy_score, precision_score, f1_score, recall
     print("Recall: " + "{:.3f}".format(recall_score))
 
 
+def save_results(method_name, accuracy_score, precision_score, f1_score, recall_score):
+    np.save('accuracy_' + method_name + '.npy', accuracy_score)
+    np.save('precision_' + method_name + '.npy', precision_score)
+    np.save('f1_' + method_name + 'npy', f1_score)
+    np.save('recall_' + method_name + '.npy', recall_score)
+
+
 # Generate samples
 X, y = make_classification(n_samples=500, weights=[0.9])
 
 # Before oversampling
 accuracy_score, precision_score, f1_score, recall_score = test_before_oversampling(X, y)
+# save_results("Before oversampling", accuracy_score, precision_score, f1_score, recall_score)
 print_results("Before oversampling", accuracy_score, precision_score, f1_score, recall_score)
 
 # Oversampling with implemented Adasyn
 adasyn = Adasyn()
 accuracy_score, precision_score, f1_score, recall_score = test_implemented_adasyn(X, y, adasyn)
+# save_results("Implemented Adasyn", accuracy_score, precision_score, f1_score, recall_score)
 print_results("Implemented Adasyn", accuracy_score, precision_score, f1_score, recall_score)
 
 # Oversampling with imported Adasyn
 ADASYN = ADASYN()
 accuracy_score, precision_score, f1_score, recall_score = test_oversampling_method(X, y, ADASYN)
+# save_results("Imported Adasyn", accuracy_score, precision_score, f1_score, recall_score)
 print_results("Imported Adasyn", accuracy_score, precision_score, f1_score, recall_score)
 
 # Oversampling with SMOTE
 SMOTE = SMOTE()
 accuracy_score, precision_score, f1_score, recall_score = test_oversampling_method(X, y, SMOTE)
+# ("SMOTE", accuracy_score, precision_score, f1_score, recall_score)
 print_results("SMOTE", accuracy_score, precision_score, f1_score, recall_score)
 
 # Oversampling with SMOTE-ENN
 SMOTE_ENN = SMOTEENN()
 accuracy_score, precision_score, f1_score, recall_score = test_oversampling_method(X, y, SMOTE_ENN)
+# save_results("SMOTE-ENN", accuracy_score, precision_score, f1_score, recall_score)
 print_results("SMOTE-ENN", accuracy_score, precision_score, f1_score, recall_score)
 
 # Oversampling with SMOTE-BORDERLINE
 BORDERLINE_SMOTE = BorderlineSMOTE()
 accuracy_score, precision_score, f1_score, recall_score = test_oversampling_method(X, y, BORDERLINE_SMOTE)
+# save_results("SMOTE-BORDERLINE", accuracy_score, precision_score, f1_score, recall_score)
 print_results("SMOTE-BORDERLINE", accuracy_score, precision_score, f1_score, recall_score)
 
 print("----------------------")
