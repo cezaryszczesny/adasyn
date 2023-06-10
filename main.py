164e44db7088b8d9606ab693506500655d3af1d3
Cezary Szczesny <c.szczesny@streamsoft.pl>
2023-06-10 19:27:03 +0200

    refaktor nazw na małe litery

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import numpy as np
 from imblearn.combine import SMOTEENN
-from imblearn.over_sampling import ADASYN, SMOTE, BorderlineSMOTE
-from scipy.stats import shapiro, stats, ttest_rel
+from imblearn.over_sampling import SMOTE, BorderlineSMOTE, ADASYN
+from implemented_adasyn import Adasyn
+from scipy.stats import ttest_rel
 from sklearn.datasets import make_classification
 
-from adasyn import Adasyn
 from testMethods import test_oversampling_method, test_before_oversampling
 
 
@@ -94,32 +94,32 @@ before_over_scores = np.array(test_before_oversampling(X, y))
 # print_results(*before_over_scores)
 
 # Oversampling with implemented Adasyn
-adasyn = Adasyn()
-impl_adasyn_scores = np.array(test_oversampling_method(X, y, adasyn))
+implemented_adasyn = Adasyn()
+impl_adasyn_scores = np.array(test_oversampling_method(X, y, implemented_adasyn))
 # save_results("Implemented Adasyn", *impl_adasyn_scores)
 # print_results(*impl_adasyn_scores)
 
 # Oversampling with imported Adasyn
-ADASYN = ADASYN()
-imported_adasyn_scores = np.array(test_oversampling_method(X, y, ADASYN))
+adasyn = ADASYN()
+imported_adasyn_scores = np.array(test_oversampling_method(X, y, adasyn))
 # save_results("Imported Adasyn", *imported_adasyn_scores)
 # print_results(*imported_adasyn_scores)
 
 # Oversampling with SMOTE
-SMOTE = SMOTE()
-smote_scores = np.array(test_oversampling_method(X, y, SMOTE))
+smote = SMOTE()
+smote_scores = np.array(test_oversampling_method(X, y, smote))
 # save_results("SMOTE", *smote_scores)
 # print_results(*smote_scores)
 
 # Oversampling with SMOTE-ENN
-SMOTE_ENN = SMOTEENN()
-smote_enn_scores = np.array(test_oversampling_method(X, y, SMOTE_ENN))
+smote_enn = SMOTEENN()
+smote_enn_scores = np.array(test_oversampling_method(X, y, smote_enn))
 # save_results("SMOTE-ENN", *smote_enn_scores)
 # print_results(*smote_enn_scores)
 
 # Oversampling with SMOTE-BORDERLINE
-BORDERLINE_SMOTE = BorderlineSMOTE()
-borderline_smote_scores = np.array(test_oversampling_method(X, y, BORDERLINE_SMOTE))
+borderline_smote = BorderlineSMOTE()
+borderline_smote_scores = np.array(test_oversampling_method(X, y, borderline_smote))
 # save_results("SMOTE-BORDERLINE", *borderline_smote_scores)
 # print_results(*borderline_smote_scores)
 
